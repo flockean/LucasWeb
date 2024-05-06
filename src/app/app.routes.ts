ecc94b68e62fd21bbb0ff78501740e67bed5d0ac
flockean <lucas.buchholz1@gmail.com>
2024-05-06 14:41:11 +0200

    ci: MyFirstPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {MainPortalComponent} from "./main-portal/main-portal.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "MainPortal",
+    component: MainPortalComponent
+  }
+];
---
+++ src/app/main-portal/main-portal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-portal',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-portal.component.html',
+  styleUrl: './main-portal.component.scss'
+})
+export class MainPortalComponent {
+
+}
