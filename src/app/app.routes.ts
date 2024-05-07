881d6ce9e6425af59181ca6fff8c07778cc6d995
flockean <lucas.buchholz1@gmail.com>
2024-05-07 14:25:13 +0200

    feat: struct-Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import {MainPortalComponent} from "./main-portal/main-portal.component";
+import {LogInPageComponent} from "./log-in-page/log-in-page.component";
 
 export const routes: Routes = [
   {
     path: "MainPortal",
     component: MainPortalComponent
+  },
+  {
+    path: "LogIn",
+    component: LogInPageComponent
   }
 ];
