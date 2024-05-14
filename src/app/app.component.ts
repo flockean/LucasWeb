0791ac77d82dbd6c1947f7ba5f8bd40fd611a28b
flockean <lucas.buchholz1@gmail.com>
2024-05-14 19:57:28 +0200

    ci: LogInComponent, Implem and Style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {MainPortalComponent} from "./main-portal/main-portal.component";
+import {NgIf} from "@angular/common";
+import {ConfigService} from "./shared/config.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'LucasWeb';
   protected readonly MainPortalComponent = MainPortalComponent;
+
+  constructor(protected configService: ConfigService) {
+  }
 }
--- src/app/log-in-page/log-in-page.component.ts
+++ src/app/log-in-page/log-in-page.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ConfigService} from "../shared/config.service";
+import {NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-log-in-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './log-in-page.component.html',
   styleUrl: './log-in-page.component.scss'
 })
 export class LogInPageComponent {
+  userForm;
+  constructor(
+    protected configService: ConfigService,
+    private formbuilder: FormBuilder
+  ) {
+    this.userForm = this.formbuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
+  // TODO: Implementation of LogIn more elegant
+  // please never implement LogIn like this!
+  onSubmit() {
+    console.log(this.userForm.value.username)
+    this.configService.login(this.userForm.value.username!, this.userForm.value.password!)
+  }
 }
+
