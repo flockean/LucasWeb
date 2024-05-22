b4e2a698177f99a8fd8dd7bba056ecc278197293
flockean <lucas.buchholz1@gmail.com>
2024-05-22 10:18:53 +0200

    ci: unused cookieMethod

--- src/app/cookie/cookie.component.ts
+++ src/app/cookie/cookie.component.ts
@@ -12,16 +12,11 @@ import {NgForOf} from "@angular/common";
   styleUrl: './cookie.component.scss'
 })
 export class CookieComponent {
-  private isConsented: boolean = false;
 
   // TODO: Do I need a Dependency for CookieService? Uhhhh
   constructor(private cookieService: CookieService) {
     this.cookieService.set('MainCookie', 'This is your Cookie, no one else!')
     this.cookieService.set('SecondCookie', "Well, this is your second Chocolate Cookie")
-  }
-
-  setCookie(name: string) {
-    this.cookieService.set('MainCookie', 'This is your Cookie, no one else!')
   }
 
   getCookies() {
