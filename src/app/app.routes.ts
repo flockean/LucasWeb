8e790f3cca3ca8eb17f23e7abcc0474c1e33001d
flockean <lucas.buchholz1@gmail.com>
2024-05-22 10:15:39 +0200

    ci: Here are your cookies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {MainPortalComponent} from "./main-portal/main-portal.component";
 import {LogInPageComponent} from "./log-in-page/log-in-page.component";
 import {AuthGuard} from "./app.guard";
+import {CookieComponent} from "./cookie/cookie.component";
 
 export const routes: Routes = [
   {
@@ -12,5 +13,9 @@ export const routes: Routes = [
   {
     path: "LogIn",
     component: LogInPageComponent
+  },
+  {
+    path: "Cookies",
+    component: CookieComponent
   }
 ];
