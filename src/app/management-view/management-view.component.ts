a92935953820a509decbd3457be670bb583ef6a8
flockean <lucas.buchholz1@gmail.com>
2024-08-17 18:59:20 +0200

    ci: managementComp

---
+++ src/app/management-view/management-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-management-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './management-view.component.html',
+  styleUrl: './management-view.component.scss'
+})
+export class ManagementViewComponent {
+
+}
