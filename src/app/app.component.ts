9b20a3d7dadbbcb591613601a2f27e95e59c494f
flockean <lucas.buchholz1@gmail.com>
2024-08-17 18:58:24 +0200

    ci: navbarcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {MainPortalComponent} from "./main-portal/main-portal.component";
 import {NgIf} from "@angular/common";
 import {ConfigService} from "./shared/config.service";
+import { NavibarComponent } from './components/navibar/navibar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NgIf],
+  imports: [RouterOutlet, RouterLink, NgIf, NavibarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
