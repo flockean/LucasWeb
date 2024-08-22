db13566f7e83f406e0a400c24d3593cf7f967747
flockean <lucas.buchholz1@gmail.com>
2024-08-22 19:09:27 +0200

    feat: project mangment structure

--- src/app/management-view/management-view.component.ts
+++ src/app/management-view/management-view.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-management-view',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './management-view.component.html',
   styleUrl: './management-view.component.scss'
 })
 export class ManagementViewComponent {
+  projectForm;
+  serviceForm;
+  constructor(private formbuilder: FormBuilder) {
+    this.serviceForm = this.formbuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
 
+    this.projectForm = this.formbuilder.group({
+      // TODO: fill Data
+    })
+
+  }
 }
