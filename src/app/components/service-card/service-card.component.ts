33c37929eaef2a372d2778ab551bc243c0aaf389
flockean <lucas.buchholz1@gmail.com>
2024-08-30 23:53:29 +0200

    ci: temporary project.json file

--- src/app/components/service-card/service-card.component.ts
+++ src/app/components/service-card/service-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Service } from '../project-card/project-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-service-card',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './service-card.component.html',
   styleUrl: './service-card.component.scss'
 })
 export class ServiceCardComponent {
 
+  constructor() {}
+  @Input() services: Service[] = []
 }
